b4e4de4a840324c0722dc5948384ea5576c7b152
93N3515 <70991773+93N3515@users.noreply.github.com>
2022-12-07 19:41:57 +0700

    Update Scrapper.py

--- Scrapper.py
+++ Scrapper.py
@@ -1,27 +1,67 @@
 import os
 import datetime
 import threading
+import zipfile
+from zipfile import ZipFile
+import rarfile
 
-def cookieParser(path):
+def cookieParser(path,work):
     savefilename = f"tokens {str(datetime.date.today()) + ' ' + str(datetime.datetime.now().hour) + '.' + str(datetime.datetime.now().minute) + '.' + str(datetime.datetime.now().second)}"
     os.mkdir(savefilename)
     print("Загрузка логов")
     tokens = []
-    for root, dirs, filename in os.walk(path):
-        for file in filename:
-            filep = root + "\\" + file
-            try:
-                fileo = open(root + "\\" + file, 'r', encoding='latin-1')
-            except:
-                print(f"произошла ошибка{fileo}")
-                pass
-            for line in fileo.readlines():
-                if line.find("	/	") >= 1:
-                    cc_split = line.split("	")
-                    if cc_split[0] == ("tiktok.com") or cc_split[0] == (".tiktok.com") or cc_split[0] == (
-                    ".www.tiktok.com") or cc_split[0] == ("www.tiktok.com"):
-                        if str(cc_split[5]) == ('sessionid_ss') or str(cc_split[5]) == ('sessionid'):
-                            tokens.append(line.split("	")[6].strip() + "_-#-_" + filep)
+
+    if work == "1":
+        for root, dirs, filename in os.walk(path):
+            for file in filename:
+                filep = root + "\\" + file
+                try:
+                    fileo = open(root + "\\" + file, 'r', encoding='latin-1')
+                except:
+                    print(f"произошла ошибка{fileo}")
+                    pass
+                for line in fileo.readlines():
+                    if line.find("	/	") >= 1:
+                        cc_split = line.split("	")
+                        if cc_split[0] == ("tiktok.com") or cc_split[0] == (".tiktok.com") or cc_split[0] == (
+                        ".www.tiktok.com") or cc_split[0] == ("www.tiktok.com"):
+                            if str(cc_split[5]) == ('sessionid_ss') or str(cc_split[5]) == ('sessionid'):
+                                tokens.append(line.split("	")[6].strip() + "_-#-_" + filep)
+
+    else:
+        for file in os.listdir(path):
+            if os.path.isdir(path+"\\"+file) != True:
+                if zipfile.is_zipfile(path+"\\"+file):
+                    with ZipFile(path+"\\"+file) as myzip:
+                        for i in ZipFile.infolist(myzip):
+                            try:
+                                if i.filename.find(".txt") >= 1:
+                                    for line in myzip.open(i).readlines():
+                                        line = line.decode()
+                                        if line.find("	/	") >= 1:
+                                            line = line.split("	")
+                                            if line[0] == ("tiktok.com") or line[0] == (".tiktok.com") or line[0] == (
+                                                    ".www.tiktok.com") or line[0] == ("www.tiktok.com"):
+                                                if str(line[5]) == ('sessionid_ss') or str(line[5]) == ('sessionid'):
+                                                    tokens.append(
+                                                        line[6].strip() + "_-#-_" + myzip.filename + str(i.filename))
+                            except:
+                                pass
+                elif rarfile.is_rarfile(path+"\\"+file):
+                    with rarfile.RarFile(path + "\\" + file) as myrar:
+                        for i in rarfile.RarFile.infolist(myrar):
+                            try:
+                                if i.filename.find(".txt") >= 1:
+                                    for line in myrar.open(i).readlines():
+                                        line = line.decode()
+                                        if line.find("	/	") >= 1:
+                                            line = line.split("	")
+                                            if line[0] == ("tiktok.com") or line[0] == (".tiktok.com") or line[0] == (
+                                            ".www.tiktok.com") or line[0] == ("www.tiktok.com"):
+                                                if str(line[5]) == ('sessionid_ss') or str(line[5]) == ('sessionid'):
+                                                    tokens.append(line[6].strip() + "_-#-_" + myrar.filename + str(i.filename))
+                            except:
+                                pass
 
     tokens = list(set(tokens))
     print(len(tokens))
@@ -38,8 +78,11 @@ def cookieParser(path):
     result.close
     result_p.close()
     print("Загрузка прошла успешно")
-def menu():
 
-    main_logs_path = cookieParser(input("Вставьте путь до папки с логами"))
+def menu():
+    work = input("Режим работы: \n1. Из папки\n2. Из архив")
+    main_logs_path = cookieParser(input("Вставьте путь до папки с логами"),work)
+
+
 
 menu()
