ec81847cbea28b2144df5f3e056d535b5d26df73
93N3515 <70991773+93N3515@users.noreply.github.com>
2022-12-09 14:53:21 +0700

    Update Scrapper.py

--- Scrapper.py
+++ Scrapper.py
@@ -29,42 +29,46 @@ def cookieParser(path,work):
                                 tokens.append(line.split("	")[6].strip() + "_-#-_" + filep)
 
     else:
-        for file in os.listdir(path):
-            if os.path.isdir(path+"\\"+file) != True:
-                if zipfile.is_zipfile(path+"\\"+file):
-                    with ZipFile(path+"\\"+file) as myzip:
-                        for i in ZipFile.infolist(myzip):
-                                if i.filename.find(".txt") >= 1:
-                                    try:
-                                        for line in myzip.open(i).readlines():
-                                            line = line.decode()
-                                            if line.find("	/	") >= 1:
-                                                line = line.split("	")
-                                                if line[0] == ("tiktok.com") or line[0] == (".tiktok.com") or line[0] == (
-                                                        ".www.tiktok.com") or line[0] == ("www.tiktok.com"):
-                                                    if str(line[5]) == ('sessionid_ss') or str(line[5]) == ('sessionid'):
-                                                        tokens.append(
-                                                            line[6].strip() + "_-#-_" + myzip.filename + str(i.filename))
-                                    except UnicodeDecodeError:
-                                        print("unicode error")
+            for file in os.listdir(path):
+                if os.path.isdir(path+"\\"+file) != True:
+                    if zipfile.is_zipfile(path+"\\"+file):
+                        try:
+                            with ZipFile(path+"\\"+file) as myzip:
+                                for i in ZipFile.infolist(myzip):
+                                        if i.filename.find(".txt") >= 1:
+                                            try:
+                                                for line in myzip.open(i).readlines():
+                                                    line = line.decode()
+                                                    if line.find("	/	") >= 1:
+                                                        line = line.split("	")
+                                                        if line[0] == ("tiktok.com") or line[0] == (".tiktok.com") or line[0] == (
+                                                                ".www.tiktok.com") or line[0] == ("www.tiktok.com"):
+                                                            if str(line[5]) == ('sessionid_ss') or str(line[5]) == ('sessionid'):
+                                                                tokens.append(
+                                                                    line[6].strip() + "_-#-_" + myzip.filename + str(i.filename))
+                                            except UnicodeDecodeError:
+                                                print("unicode error")
+                        except RuntimeError:
+                            print(f"\t{file}\t Имеется пароль")
 
-                elif rarfile.is_rarfile(path+"\\"+file):
-                    with rarfile.RarFile(path + "\\" + file) as myrar:
-                        for i in rarfile.RarFile.infolist(myrar):
-                            if i.filename.find(".txt") >= 1:
-                                try:
-                                    for line in myrar.open(i).readlines():
-                                        line = line.decode()
-                                        if line.find("	/	") >= 1:
-                                            line = line.split("	")
-                                            if line[0] == ("tiktok.com") or line[0] == (".tiktok.com") or line[0] == (
-                                            ".www.tiktok.com") or line[0] == ("www.tiktok.com"):
-                                                if str(line[5]) == ('sessionid_ss') or str(line[5]) == ('sessionid'):
-                                                    tokens.append(line[6].strip() + "_-#-_" + myrar.filename + str(i.filename))
-                                except UnicodeDecodeError:
-                                    print("unicode error")
-
-
+                    elif rarfile.is_rarfile(path+"\\"+file):
+                        try:
+                            with rarfile.RarFile(path + "\\" + file) as myrar:
+                                for i in rarfile.RarFile.infolist(myrar):
+                                    if i.filename.find(".txt") >= 1:
+                                        try:
+                                            for line in myrar.open(i).readlines():
+                                                line = line.decode()
+                                                if line.find("	/	") >= 1:
+                                                    line = line.split("	")
+                                                    if line[0] == ("tiktok.com") or line[0] == (".tiktok.com") or line[0] == (
+                                                    ".www.tiktok.com") or line[0] == ("www.tiktok.com"):
+                                                        if str(line[5]) == ('sessionid_ss') or str(line[5]) == ('sessionid'):
+                                                            tokens.append(line[6].strip() + "_-#-_" + myrar.filename + str(i.filename))
+                                        except UnicodeDecodeError:
+                                            print("unicode error")
+                        except rarfile.PasswordRequired:
+                            print(f"\t{file}\t Имеется пароль")
     tokens = list(set(tokens))
     print(len(tokens))
     result = open(f'{savefilename}\\result.txt', 'w+', encoding="UTF-8")
